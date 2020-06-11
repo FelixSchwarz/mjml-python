e22e25270677a2e3320c20e19ce3a22682f09c66
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-06-11 10:51:03 +0200

    use single generic test to compare generated HTML with upstream

---
+++ tests/upstream_alignment_test.py
@@ -0,0 +1,34 @@
+
+from pathlib import Path
+from unittest import TestCase
+
+from ddt import ddt as DataDrivenTestCase, data as ddt_data
+from htmlcompare import assert_same_html
+
+from mjml import mjml_to_html
+
+
+
+TESTDATA_DIR = Path(__file__).parent / 'testdata'
+
+@DataDrivenTestCase
+class UpstreamAlignmentTest(TestCase):
+    @ddt_data(
+        'minimal',
+        'hello-world',
+        'button',
+        'text_with_html',
+    )
+    def test_ensure_same_html(self, test_id):
+        mjml_filename = f'{test_id}.mjml'
+        html_filename = f'{test_id}-expected.html'
+        with (TESTDATA_DIR / html_filename).open('rb') as html_fp:
+            expected_html = html_fp.read()
+
+        with (TESTDATA_DIR / mjml_filename).open('rb') as mjml_fp:
+            result = mjml_to_html(mjml_fp)
+
+        assert not result.errors
+        actual_html = result.html
+        assert_same_html(expected_html, actual_html)
+
