d7f56b40eed5517c10b8d951acd45107162890ad
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-06-11 11:01:25 +0200

    implement <mj-title>

--- tests/upstream_alignment_test.py
+++ tests/upstream_alignment_test.py
@@ -18,6 +18,7 @@ class UpstreamAlignmentTest(TestCase):
         'hello-world',
         'button',
         'text_with_html',
+        'mj-title',
     )
     def test_ensure_same_html(self, test_id):
         mjml_filename = f'{test_id}.mjml'
