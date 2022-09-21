4ae9d127bb15f6564a5202dfd8d6ad325f2c54d9
Casey Holzer <casey.holzer@hospiq.com>
2022-09-21 12:33:28 -0600

    add mj-wrapper

---
+++ mjml/elements/mj_wrapper.py
@@ -0,0 +1,40 @@
+
+from . import MjSection
+from ..helpers import suffixCssClasses
+
+
+__all__ = ['MjWrapper']
+
+
+class MjWrapper(MjSection):
+    def renderWrappedChildren(self):
+        children = self.props['children']
+        containerWidth = self.context['containerWidth']
+
+        def render_child(component):
+            if component.isRawElement():
+                return component.render()
+            td_ie_attrs = component.html_attrs(
+                # TODO: no component has an "align" attr, also never used?
+                #align=component.get_attr('align'),
+                class_=suffixCssClasses(
+                      component.get_attr('css-class'),
+                      'outlook',
+                    ),
+                width=containerWidth,
+            )
+            return f'''
+              <!--[if mso | IE]>
+                <tr>
+                  <td
+                    {td_ie_attrs}
+                  >
+              <![endif]-->
+                {component.render()}
+              <!--[if mso | IE]>
+                  </td>
+                 </tr>
+              <![endif]-->
+            '''
+
+        return self.renderChildren(children, renderer=render_child)
