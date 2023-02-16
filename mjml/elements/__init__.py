b7f5a3122850a3c38a9b45724d0e8bb367ddf9e6
Casey Holzer <casey.h@leantaas.com>
2023-02-15 17:43:29 -0700

    add mj-hero

--- mjml/elements/__init__.py
+++ mjml/elements/__init__.py
@@ -8,6 +8,7 @@ from .mj_button import *
 from .mj_column import *
 from .mj_divider import *
 from .mj_group import *
+from .mj_hero import *
 from .mj_image import *
 from .mj_navbar import *
 from .mj_navbar_link import *
