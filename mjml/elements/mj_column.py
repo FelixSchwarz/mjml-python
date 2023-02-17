FelixSchwarz/mjml-python

commit 910812b1cb941747892b722428dc729cda70c8a7

make missing_ok keyword-only

mjml/elements/mj_column.py
@@ -205,9 +205,9 @@ class MjColumn(BodyComponent):
             if component.isRawElement():
                 return component.render()
             td_attrs = component.html_attrs(
-                align = component.getAttribute('align', True),
+                align = component.getAttribute('align', missing_ok=True),
                 # vertical-align
-                class_ = component.getAttribute('css-class', True),
+                class_ = component.getAttribute('css-class', missing_ok=True),
                 style = {
                     'background': component.getAttribute(
                       'container-background-color',
@@ -221,7 +221,7 @@ class MjColumn(BodyComponent):
                     'padding-left': component.getAttribute('padding-left'),
                     'word-break': 'break-word',
                   },
-                **{'vertical-align': component.getAttribute('vertical-align', True)}
+                **{'vertical-align': component.getAttribute('vertical-align', missing_ok=True)}
             )
             return f'''<tr>
               <td {td_attrs}>

mjml/core/api.py
@@ -77,7 +77,7 @@ class Component:
         return self.context
 
     # js: getAttribute(name)
-    def get_attr(self, name, missing_ok=False):
+    def get_attr(self, name, *, missing_ok=False):
         if not missing_ok and (name not in self.allowed_attrs()) and (name not in self.default_attrs()):
             raise AssertionError(f'{self.__class__.__name__} has no declared attr {name}')
         return self.attrs.get(name)

mjml/elements/mj_navbar_link.py
@@ -74,9 +74,9 @@ class MjNavbarLink(BodyComponent):
 
     def renderContent(self):
         href = self.getAttribute('href')
-        navbar_base_url = self.getAttribute('navbarBaseUrl', True)
+        navbar_base_url = self.getAttribute('navbarBaseUrl', missing_ok=True)
         link = f'{navbar_base_url}{href}' if navbar_base_url else href
-        css_class = self.getAttribute('css-class', True) or ''
+        css_class = self.getAttribute('css-class', missing_ok=True) or ''
         html_attrs = self.html_attrs(
             class_=f'mj-link {css_class}',
             href=link,
@@ -96,7 +96,7 @@ class MjNavbarLink(BodyComponent):
         html_attrs = self.html_attrs(
             style='td',
             class_=suffixCssClasses(
-                self.getAttribute('css-class', True),
+                self.getAttribute('css-class', missing_ok=True),
                 'outlook',
             ),
         )

commit 2e8f7f7c763ce1e16fc40810a8231c47fb7e7ea3

Component.get_attr(): "missing_ok" is a keyword-only parameter
Fixes: 910812b1 ("make missing_ok keyword-only")

mjml/elements/mj_column.py
@@ -211,7 +211,7 @@ class MjColumn(BodyComponent):
                 style = {
                     'background': component.getAttribute(
                       'container-background-color',
-                        True
+                      missing_ok=True
                     ),
                     'font-size': '0px',
                     'padding': component.getAttribute('padding'),

mjml/elements/mj_section.py
@@ -244,7 +244,7 @@ class MjSection(BodyComponent):
             if component.isRawElement():
                 return component.render()
             td_ie_attrs = component.html_attrs(
-                align=component.get_attr('align', True),
+                align=component.get_attr('align', missing_ok=True),
                 class_=suffixCssClasses(
                       component.get_attr('css-class'),
                       'outlook',

mjml/elements/mj_wrapper.py
@@ -15,7 +15,7 @@ class MjWrapper(MjSection):
             if component.isRawElement():
                 return component.render()
             td_ie_attrs = component.html_attrs(
-                align=component.get_attr('align', True),
+                align=component.get_attr('align', missing_ok=True),
                 class_=suffixCssClasses(
                       component.get_attr('css-class'),
                       'outlook',