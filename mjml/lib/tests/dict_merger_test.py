70e072fb84770ad36ca5dce19d2255597a142ba6
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2023-11-18 08:07:04 +0100

    use only pytest for testing
    pytest is clearly the preferred tool for testing in Python.

--- mjml/lib/tests/dict_merger_test.py
+++ mjml/lib/tests/dict_merger_test.py
@@ -2,23 +2,20 @@
 # Copyright 2015 Felix Schwarz
 # The source code in this file is licensed under the MIT license.
 
-from pythonic_testcase import *
-
 from ..dict_merger import merge_dicts
 
 
-class DictMergerTest(PythonicTestCase):
-    def test_returns_single_dict_unmodified(self):
-        assert_equals({}, merge_dicts({}))
-        assert_equals({'bar': 42}, merge_dicts({'bar': 42}))
+def test_returns_single_dict_unmodified():
+    assert merge_dicts({}) == {}
+    assert merge_dicts({'bar': 42}) == {'bar': 42}
 
-    def test_can_merge_two_dicts_without_modifying_inputs(self):
-        a = {'a': 1}
-        b = {'b': 2}
-        assert_equals({'a': 1, 'b': 2}, merge_dicts(a, b))
+def test_can_merge_two_dicts_without_modifying_inputs():
+    a = {'a': 1}
+    b = {'b': 2}
+    assert merge_dicts(a, b) == {'a': 1, 'b': 2}
 
-    def test_can_merge_three_dicts_without_modifying_inputs(self):
-        a = {'a': 1}
-        b = {'b': 2}
-        c = {'c': 3}
-        assert_equals({'a': 1, 'b': 2, 'c': 3}, merge_dicts(a, b, c))
+def test_can_merge_three_dicts_without_modifying_inputs():
+    a = {'a': 1}
+    b = {'b': 2}
+    c = {'c': 3}
+    assert merge_dicts(a, b, c) == {'a': 1, 'b': 2, 'c': 3}
