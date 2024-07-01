40d70aab65d8a61750a0e8995e46f5cb650db7f8
Dan Lindholm <git@dan.mk>
2024-07-01 23:06:05 +0200

    fix redundant parsing

--- mjml/elements/mj_divider.py
+++ mjml/elements/mj_divider.py
@@ -63,7 +63,7 @@ class MjDivider(BodyComponent):
         parsedWidth, unit = widthParser(width)
 
         if unit == '%':
-            px = (parse_int(containerWidth) * parse_int(parsedWidth)) / 100 - paddingSize
+            px = (parse_int(containerWidth) * parsedWidth) / 100 - paddingSize
             # we want to render the number as string without decimal digits if possible
             if px == int(px):
                 px = int(px)
