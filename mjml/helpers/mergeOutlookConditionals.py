b84642a87d92d95c5d9ebf9ca2e3c74f6435d439
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2025-05-13 10:45:04 +0200

    use `re.sub(..., flags=re.MULTILINE)` instead of passing `re.MULTILINE` as a positional parameter
    Previously the flag was passed to the `count` parameter. Thankfully this was found
    by a deprecation warning Python 3.13.
    
    Fixes: d82c85f0 ("initial version which does /something/: The "Hello World" example should render correctly")

--- mjml/helpers/mergeOutlookConditionals.py
+++ mjml/helpers/mergeOutlookConditionals.py
@@ -7,4 +7,4 @@ __all__ = ['mergeOutlookConditionals']
 # OPTIMIZE ME: — check if previous conditional is `<!--[if mso | I`]>` too
 def mergeOutlookConditionals(content):
     # re.sub() replaces all occurrences by default ("g" modifier in JavaScript)
-    return re.sub(r'(<!\[endif]-->\s*?<!--\[if mso \| IE]>)', '', content, re.MULTILINE)
+    return re.sub(r'(<!\[endif]-->\s*?<!--\[if mso \| IE]>)', '', content, flags=re.MULTILINE)

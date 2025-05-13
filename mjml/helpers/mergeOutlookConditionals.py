FelixSchwarz/mjml-python

commit d82c85f07eacd3a03d4d1a4811e81444cb8a2d9b

initial version which does /something/: The "Hello World" example should render correctly

mjml/helpers/mergeOutlookConditionals.py
@@ -1,0 +1,10 @@
+
+import re
+
+__all__ = ['mergeOutlookConditionnals']
+
+# OPTIMIZE ME: — check if previous conditionnal is `<!--[if mso | I`]>` too
+def mergeOutlookConditionnals(content):
+    # re.sub() replaces all occurrences by default ("g" modifier in JavaScript)
+    return re.sub('(<!\[endif]-->\s*?<!--\[if mso \| IE]>)', '', content, re.MULTILINE)
+

.travis.yml
@@ -12,5 +12,5 @@ install:
     - pip install nose
     - python setup.py install
 
-script: nosetests -s
+script: nosetests -s tests/*/*.py
 

mjml/__init__.py
@@ -1,0 +1,3 @@
+
+from .mjml2html import mjml_to_html
+

mjml/core/__init__.py
@@ -1,0 +1,2 @@
+
+from .components import *

mjml/core/components.py
@@ -1,0 +1,29 @@
+
+
+__all__ = ['initComponent']
+
+def _components():
+    from ..elements import MjText, MjSection, MjColumn, MjBody, MjImage, MjDivider
+    components = {
+        'mj-text': MjText,
+        'mj-divider': MjDivider,
+        'mj-image': MjImage,
+        'mj-section': MjSection,
+        'mj-column': MjColumn,
+        'mj-body': MjBody,
+    }
+    return components
+
+def initComponent(initialDatas, name):
+    components = _components()
+    component_cls = components[name]
+    if not component_cls:
+        return None
+
+    component = component_cls(**initialDatas)
+    if getattr(component, 'headStyle', None):
+        component.context['addHeadStyle'](name, component.headStyle)
+    if getattr(component, 'componentHeadStyle', None):
+        component.context['addComponentHeadSyle'](component.componentHeadStyle)
+    return component
+

mjml/elements/__init__.py
@@ -1,0 +1,8 @@
+
+from .mj_body import *
+from .mj_column import *
+from .mj_divider import *
+from .mj_image import *
+from .mj_section import *
+from .mj_text import *
+

mjml/elements/_base.py
@@ -1,0 +1,179 @@
+
+
+from ..lib import merge_dicts, AttrDict
+from ..core import initComponent
+from ..core.components import _components
+from ..helpers import *
+
+
+__all__ = [
+    'BodyComponent',
+]
+
+class Component:
+    def __init__(self, *, attributes=None, children=(), content='', context=None, props=None, globalAttributes=None, headStyle=None):
+        self.attrs = merge_dicts(self.default_attrs(), attributes or {})
+        self.children = list(children)
+        self.content = content
+        self.context = context
+
+        self.props = AttrDict(merge_dicts(props, {'children': children, 'content': content}))
+
+        # upstream also checks "self.allowed_attrs"
+        self.attributes = merge_dicts(
+            self.default_attrs(),
+            globalAttributes or {},
+            attributes or {},
+        )
+
+        # optional attributes (methods) for some components
+        if headStyle:
+            self.headStyle = headStyle
+
+    @classmethod
+    def getTagName(cls):
+        cls_name = cls.__name__
+        return cls_name
+
+    @classmethod
+    def isRawElement(cls):
+        cls_value = getattr(cls, 'rawElement', None)
+        return bool(cls_value)
+
+    # js: static defaultAttributes
+    @classmethod
+    def default_attrs(cls):
+        return {}
+
+    def getChildContext(self):
+        return self.context
+
+    # js: getAttribute(name)
+    def get_attr(self, name):
+        # assert name in allowed_attrs
+        return self.attrs[name]
+    getAttribute = get_attr
+
+    def render(self):
+        return ''
+
+
+
+class BodyComponent(Component):
+    def render(self):
+        raise NotImplementedError(f'{self.__cls__.__name__} should override ".render()"')
+
+    def getShorthandAttrValue(self, attribute, direction):
+        mjAttributeDirection = self.getAttribute(f'{attribute}-{direction}')
+        mjAttribute = self.getAttribute(attribute)
+
+        if mjAttributeDirection:
+            return strip_unit(mjAttributeDirection)
+        if not mjAttribute:
+            return 0
+        return shorthandParser(mjAttribute, direction)
+
+    def getShorthandBorderValue(self, direction):
+        borderDirection = direction and self.getAttribute(f'border-{direction}')
+        border = self.getAttribute('border')
+        return borderParser(borderDirection or border or '0')
+
+    def getBoxWidths(self):
+        containerWidth = self.context['containerWidth']
+        parsedWidth = strip_unit(containerWidth)
+        paddings = self.getShorthandAttrValue('padding', 'right') + self.getShorthandAttrValue('padding', 'left')
+        borders = self.getShorthandBorderValue('right') + self.getShorthandBorderValue('left')
+
+        return {
+            'totalWidth': parsedWidth,
+            'borders'   : borders,
+            'paddings'  : paddings,
+            'box'       : parsedWidth - paddings - borders,
+        }
+
+    def getContent(self):
+        return self.content.strip()
+
+    # js: htmlAttributes(attributes)
+    def html_attrs(self, **attrs):
+        def _to_str(kv):
+            key, value = kv
+            if key == 'style':
+                value = self.styles(value)
+            if key == 'class_':
+                key = 'class'
+            return f'{key}="{value}"' if is_not_empty(value) else None
+        serialized_attrs = map(_to_str, attrs.items())
+        return ' '.join(filter(None, serialized_attrs))
+
+    # js: getStyles()
+    def get_styles(self):
+        return {}
+
+    # js: styles(styles)
+    def styles(self, key=None):
+        _styles = None
+        if key and isinstance(key, str):
+            _styles_dict = self.get_styles()
+            _styles = _styles_dict.get(key)
+            if _styles and not isinstance(_styles, dict):
+                raise ValueError(f'key={key}')
+        elif key:
+            # predefined dict
+            _styles = key
+        if not _styles:
+            _styles = {}
+
+        def serializer(kv):
+            k, v = kv
+            return f'{k}:{v}' if is_not_empty(v) else None
+        style_attr_strs = filter(None, map(serializer, _styles.items()))
+        style_str = ';'.join(style_attr_strs)
+        return style_str
+
+    def renderChildren(self, childrens=None, props=None, renderer=None, attributes=None, rawXML=False):
+        if not props:
+            props = {}
+        if not renderer:
+            renderer = lambda component: component.render()
+        if not attributes:
+            attributes = {}
+        childrens = childrens or self.props.children
+
+        if rawXML:
+            # return childrens.map(child => jsonToXML(child)).join('\n')
+            raise NotImplementedError
+        sibling = len(childrens)
+        rawComponents = tuple(filter(lambda c: c.isRawElement(), _components().values()))
+        def has_non_raw_children(child):
+            has_same_tag = lambda c: (c.getTagName() == child.tagName)
+            return any(filter(has_same_tag, rawComponents))
+        _nonRawSiblings = tuple(filter(has_non_raw_children, childrens))
+        nonRawSiblings = len(_nonRawSiblings)
+
+        output = ''
+        index = 0
+        for children in childrens:
+            child_props = merge_dicts(props, {
+                'first': (index == 0),
+                'index': index,
+                'last': (index+1 == sibling),
+                'sibling': sibling,
+                'nonRawSiblings': nonRawSiblings,
+            })
+            initialDatas = merge_dicts(children,{
+                'attributes': merge_dicts(attributes, children['attributes']),
+                'context': self.getChildContext(),
+                'props': child_props,
+            })
+            initialDatas.pop('tagName')
+
+            component = initComponent(
+                name = children['tagName'],
+                initialDatas = initialDatas,
+            )
+            if component:
+                output += renderer(component)
+            index += 1
+        return output
+

mjml/elements/mj_body.py
@@ -1,0 +1,39 @@
+
+from ._base import BodyComponent
+from ..lib import merge_dicts
+
+
+__all__ = ['MjBody']
+
+class MjBody(BodyComponent):
+    @classmethod
+    def default_attrs(cls):
+        return {
+            'width'      : '600px',
+
+            # other attrs
+            'background-color': '',
+            'css-class': None,
+        }
+
+    def get_styles(self):
+        return {
+            'div': {
+                'background-color': self.get_attr('background-color'),
+            },
+        }
+
+    def getChildContext(self):
+        return merge_dicts(
+            self.context,
+            {'containerWidth': self.get_attr('width')}
+        )
+
+    def render(self):
+        setBackgroundColor = self.context['setBackgroundColor']
+        setBackgroundColor(self.get_attr('background-color'))
+
+        html_attrs = self.html_attrs(class_=self.get_attr('css-class'), style='div')
+        children_str = self.renderChildren()
+        return f'<div {html_attrs}>{children_str}</div>'
+

commit b84642a87d92d95c5d9ebf9ca2e3c74f6435d439

use `re.sub(..., flags=re.MULTILINE)` instead of passing `re.MULTILINE` as a positional parameter
Previously the flag was passed to the `count` parameter. Thankfully this was found
by a deprecation warning Python 3.13.

Fixes: d82c85f0 ("initial version which does /something/: The "Hello World" example should render correctly")

mjml/helpers/mergeOutlookConditionals.py
@@ -7,4 +7,4 @@ __all__ = ['mergeOutlookConditionals']
 # OPTIMIZE ME: — check if previous conditional is `<!--[if mso | I`]>` too
 def mergeOutlookConditionals(content):
     # re.sub() replaces all occurrences by default ("g" modifier in JavaScript)
-    return re.sub(r'(<!\[endif]-->\s*?<!--\[if mso \| IE]>)', '', content, re.MULTILINE)
+    return re.sub(r'(<!\[endif]-->\s*?<!--\[if mso \| IE]>)', '', content, flags=re.MULTILINE)