FelixSchwarz/mjml-python

commit eec9ff4a68973245ead2fd447a47b591ad4fb08c

add mjml-carousel

mjml/elements/mj_carousel.py
@@ -1,0 +1,387 @@
+
+import random
+import string
+from itertools import repeat
+
+from ..helpers import msoConditionalTag, widthParser
+from ._base import BodyComponent
+
+
+__all__ = ['MjCarousel']
+
+
+class MjCarousel(BodyComponent):
+    component_name = 'mj-carousel'
+
+    @classmethod
+    def allowed_attrs(cls):
+        return {
+            'align'                     : 'enum(left,center,right)',
+            'border-radius'             : 'unit(px,%){1,4}',
+            'container-background-color': 'color',
+            'icon-width'                : 'unit(px,%)',
+            'left-icon'                 : 'string',
+            'padding'                   : 'unit(px,%){1,4}',
+            'padding-top'               : 'unit(px,%)',
+            'padding-bottom'            : 'unit(px,%)',
+            'padding-left'              : 'unit(px,%)',
+            'padding-right'             : 'unit(px,%)',
+            'right-icon'                : 'string',
+            'thumbnails'                : 'enum(visible,hidden)',
+            'tb-border'                 : 'string',
+            'tb-border-radius'          : 'unit(px,%)',
+            'tb-hover-border-color'     : 'color',
+            'tb-selected-border-color'  : 'color',
+            'tb-width'                  : 'unit(px,%)',
+        }
+
+    @classmethod
+    def default_attrs(cls):
+        return {
+            'align'                   : 'center',
+            'border-radius'           : '6px',
+            'icon-width'              : '44px',
+            'left-icon'               : 'https://i.imgur.com/xTh3hln.png',
+            'right-icon'              : 'https://i.imgur.com/os7o9kz.png',
+            'thumbnails'              : 'visible',
+            'tb-border'               : '2px solid transparent',
+            'tb-border-radius'        : '6px',
+            'tb-hover-border-color'   : '#fead0d',
+            'tb-selected-border-color': '#ccc',
+        }
+
+    carouselId = ''.join(random.choices(string.digits, k=16))
+
+    def componentHeadStyle(self, breakpoint):
+        length = len(self.props['children'])
+        carouselId = self.carouselId
+
+        if not length:
+            return ''
+
+        def buildCssSelectors(parent, repeatCount, sibling):
+            ','.join(map(lambda i: f'''
+                {parent(i)} {repeat(
+                    '+ * ',
+                    repeatCount(i),
+                    )}+ {sibling(i)}
+            ''', range(length)))
+
+        carouselCss = f'''
+            .mj-carousel {{
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                user-select: none;
+            }}
+
+            .mj-carousel-{carouselId}-icons-cell {{
+                display: table-cell !important;
+                width: {self.getAttribute('icon-width')} !important;
+            }}
+
+            .mj-carousel-radio,
+            .mj-carousel-next,
+            .mj-carousel-previous {{
+                display: none !important;
+            }}
+
+            .mj-carousel-thumbnail,
+            .mj-carousel-next,
+            .mj-carousel-previous {{
+                touch-action: manipulation;
+            }}
+
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-radio:checked',
+                lambda i: i,
+                lambda i: '.mj-carousel-content .mj-carousel-image'
+            )} {{
+                display: none !important;
+            }}
+
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-radio-{i + 1}:checked',
+                lambda i: length - i - 1,
+                lambda i: f'.mj-carousel-content .mj-carousel-image-{i + 1}'
+            )} {{
+                display: block !important;
+            }}
+
+            .mj-carousel-previous-icons,
+            .mj-carousel-next-icons,
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-radio-{i + 1}:checked',
+                lambda i: length - i - 1,
+                lambda i: f'.mj-carousel-content .mj-carousel-next-{((i + (1 % length) + length) % length) + 1}'
+            )},
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-radio-{i + 1}:checked',
+                lambda i: length - i - 1,
+                lambda i: f'.mj-carousel-content .mj-carousel-previous-{((i - (1 % length) + length) % length) + 1}'
+            )} {{
+                display: block !important;
+            }}
+
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-radio-{i + 1}:checked',
+                lambda i: length - i - 1,
+                lambda i: f'.mj-carousel-content .mj-carousel-{carouselId}-thumbnail-{i + 1}'
+            )} {{
+                border-color: {self.getAttribute('tb-selected-border-color')} !important;
+            }}
+
+            .mj-carousel-image img + div,
+            .mj-carousel-thumbnail img + div {{
+                display: none !important;
+            }}
+
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-thumbnail:hover',
+                lambda i: length - i - 1,
+                lambda i: '.mj-carousel-main .mj-carousel-image'
+            )} {{
+                display: none !important;
+            }}
+
+            .mj-carousel-thumbnail:hover {{
+                border-color: {self.getAttribute('tb-hover-border-color')} !important;
+            }}
+
+            {buildCssSelectors(
+                lambda i: f'.mj-carousel-{carouselId}-thumbnail-{i + 1}:hover',
+                lambda i: length - i - 1,
+                lambda i: f'.mj-carousel-main .mj-carousel-image-{i + 1}'
+            )} {{
+                display: block !important;
+            }}
+        ''' # noqa: E501
+
+        fallback = f'''
+            .mj-carousel noinput {{ display:block !important; }}
+            .mj-carousel noinput .mj-carousel-image-1 {{ display: block !important;  }}
+            .mj-carousel noinput .mj-carousel-arrows,
+            .mj-carousel noinput .mj-carousel-thumbnails {{ display: none !important; }}
+
+            [owa] .mj-carousel-thumbnail {{ display: none !important; }}
+
+            @media screen yahoo {{
+                .mj-carousel-{carouselId}-icons-cell,
+                .mj-carousel-previous-icons,
+                .mj-carousel-next-icons {{
+                    display: none !important;
+                }}
+
+                {buildCssSelectors(
+                    lambda i: f'.mj-carousel-{carouselId}-radio-1:checked',
+                    lambda i: length - 1,
+                    lambda i: f'.mj-carousel-content .mj-carousel-{carouselId}-thumbnail-1'
+                )} {{
+                    border-color: transparent;
+                }}
+            }}
+        '''
+
+        return f'{carouselCss}\n{fallback}'
+
+    def get_styles(self):
+        return {
+            'carousel': {
+                'div'  : {
+                    'display'     : 'table',
+                    'width'       : '100%',
+                    'table-layout': 'fixed',
+                    'text-align'  : 'center',
+                    'font-size'   : '0px',
+                },
+                'table': {
+                    'caption-side': 'top',
+                    'display'     : 'table-caption',
+                    'table-layout': 'fixed',
+                    'width'       : '100%',
+                },
+            },
+            'images'  : {
+                'td': {
+                    'padding': '0px',
+                },
+            },
+            'controls': {
+                'div': {
+                    'display' : 'none',
+                    'mso-hide': 'all',
+                },
+                'img': {
+                    'display': 'block',
+                    'width'  : self.getAttribute('icon-width'),
+                    'height' : 'auto',
+                },
+                'td' : {
+                    'font-size': '0px',
+                    'display'  : 'none',
+                    'mso-hide' : 'all',
+                    'padding'  : '0px',
+                },
+            },
+        }
+
+    def thumbnailsWidth(self):
+        childrenLen = len(self.props['children'])
+
+        if not childrenLen:
+            return 0
+
+        parentWidth, _ = widthParser(self.context['containerWidth'])
+
+        return self.getAttribute('tb-width') or f'{min([parentWidth / childrenLen, 110])}px'
+
+    def imagesAttributes(self):
+        return map(lambda c: c['attributes'], self.children)
+
+    def generateRadios(self):
+        children = self.props['children']
+
+        return self.renderChildren(children,
+            renderer=lambda component: component.renderRadio(),
+            attributes={
+                'carouselId': self.carouselId,
+            },
+        )
+
+    def generateThumbnails(self):
+        children = self.props['children']
+
+        if self.getAttribute('thumbnails') != 'visible':
+            return ''
+
+        return self.renderChildren(children,
+            attributes={
+                'tb-border'       : self.getAttribute('tb-border'),
+                'tb-border-radius': self.getAttribute('tb-border-radius'),
+                'tb-width'        : self.thumbnailsWidth(),
+                'carouselId'      : self.carouselId,
+            },
+            renderer=lambda component: component.renderThumbnail(),
+        )
+
+    def generateControls(self, direction, icon):
+        iconWidth, _ = widthParser(self.getAttribute('icon-width'))
+
+        content = ''.join(map(
+            lambda i: f'''
+                <label
+                    {self.html_attrs(
+                        for_=f'mj-carousel-{self.carouselId}-radio-{i}',
+                        class_=f'mj-carousel-{direction} mj-carousel-{direction}-{i}',
+                    )}
+                >
+                    <img
+                        {self.html_attrs(
+                            src=icon,
+                            alt=direction,
+                            style='controls.img',
+                            width=iconWidth
+                        )}
+                    />
+                </label>
+            ''',
+            range(1, len(self.props['children']) + 1)
+        ))
+
+        return f'''
+            <td
+                {self.html_attrs(
+                    class_=f'mj-carousel-{self.carouselId}-icons-cell',
+                    style='controls.td',
+                )}
+            >
+                <div
+                    {self.html_attrs(
+                        class_=f'mj-carousel-{direction}-icons',
+                        style='controls.div',
+                    )}
+                >
+                    {content}
+                </div>
+            </td>
+        '''
+
+    def generateImages(self):
+        return f'''
+            <td
+                {self.html_attrs(style='images.td')}
+            >
+                <div
+                    {self.html_attrs(class_='mj-carousel-images')}
+                >
+                    {self.renderChildren(self.props['children'],
+                        attributes={
+                            'border-radius': self.getAttribute('border-radius'),
+                        },
+                    )}
+                </div>
+            </td>
+        '''
+
+    def generateCarousel(self):
+        return f'''
+            <table
+                {self.html_attrs(
+                    style='carousel.table',
+                    border='0',
+                    cellpadding='0',
+                    cellspacing='0',
+                    width='100%',
+                    role='presentation',
+                    class_='mj-carousel-main',
+                )}
+            >
+                <tbody>
+                    <tr>
+                        {self.generateControls('previous', self.getAttribute('left-icon'))}
+                        {self.generateImages()}
+                        {self.generateControls('next', self.getAttribute('right-icon'))}
+                    </tr>
+                </tbody>
+            </table>
+        '''
+
+    def renderFallback(self):
+        children = self.props['children']
+
+        if len(children) == 0:
+            return ''
+
+        return msoConditionalTag(
+            self.renderChildren(
+                [children[0]],
+                attributes={
+                    'border-radius': self.getAttribute('border-radius'),
+                }
+            )
+        )
+
+    def render(self):
+        content = msoConditionalTag(
+            f'''
+                <div
+                    {self.html_attrs(class_='mj-carousel')}
+                >
+                    {self.generateRadios()}
+                    <div
+                        {self.html_attrs(
+                            class_=f'mj-carousel-content mj-carousel-{self.carouselId}-content',
+                            style='carousel.div',
+                        )}
+                    >
+                        {self.generateThumbnails()}
+                        {self.generateCarousel()}
+                    </div>
+                </div>
+            ''',
+            True
+        )
+
+        return f'''
+            {content}
+            {self.renderFallback()}
+        '''

mjml/core/registry.py
@@ -15,6 +15,8 @@ def register_core_components():
         MjAccordionTitle,
         MjBody,
         MjButton,
+        MjCarousel,
+        MjCarouselImage,
         MjColumn,
         MjDivider,
         MjGroup,
@@ -47,6 +49,8 @@ def register_core_components():
         MjAccordionText,
         MjAccordionTitle,
         MjButton,
+        MjCarousel,
+        MjCarouselImage,
         MjText,
         MjDivider,
         MjHero,

mjml/elements/__init__.py
@@ -5,6 +5,8 @@ from .mj_accordion_text import *
 from .mj_accordion_title import *
 from .mj_body import *
 from .mj_button import *
+from .mj_carousel import *
+from .mj_carousel_image import *
 from .mj_column import *
 from .mj_divider import *
 from .mj_group import *

mjml/elements/_base.py
@@ -71,7 +71,10 @@ class BodyComponent(Component):
         _styles = None
         if key and isinstance(key, str):
             _styles_dict = self.get_styles()
-            _styles = _styles_dict.get(key)
+            keys = key.split('.')
+            _styles = _styles_dict.get(keys[0])
+            if len(keys) > 1:
+                _styles = _styles.get(keys[1])
             if _styles and not isinstance(_styles, dict):
                 raise ValueError(f'key={key}')
         elif key:

mjml/elements/mj_carousel_image.py
@@ -1,0 +1,151 @@
+
+
+from ..helpers import suffixCssClasses, widthParser
+from ._base import BodyComponent
+
+
+__all__ = ['MjCarouselImage']
+
+
+class MjCarouselImage(BodyComponent):
+    component_name = 'mj-carousel-image'
+
+    @classmethod
+    def allowed_attrs(cls):
+        return {
+            'alt'             : 'string',
+            'href'            : 'string',
+            'rel'             : 'string',
+            'target'          : 'string',
+            'title'           : 'string',
+            'src'             : 'string',
+            'thumbnails-src'  : 'string',
+            'border-radius'   : 'unit(px,%){1,4}',
+            'tb-border'       : 'string',
+            'tb-border-radius': 'unit(px,%){1,4}',
+        }
+
+    @classmethod
+    def default_attrs(cls):
+        return {
+            'target': '_blank',
+        }
+
+    def get_styles(self):
+        return {
+            'images'    : {
+                'img'          : {
+                    'border-radius': self.getAttribute('border-radius'),
+                    'display'      : 'block',
+                    'width'        : self.context['containerWidth'],
+                    'max-width'    : '100%',
+                    'height'       : 'auto',
+                },
+                'firstImageDiv': {},
+                'otherImageDiv': {
+                    'display' : 'none',
+                    'mso-hide': 'all',
+                },
+            },
+            'radio'     : {
+                'input': {
+                    'display' : 'none',
+                    'mso-hide': 'all',
+                },
+            },
+            'thumbnails': {
+                'a'  : {
+                    'border'       : self.getAttribute('tb-border'),
+                    'border-radius': self.getAttribute('tb-border-radius'),
+                    'display'      : 'inline-block',
+                    'overflow'     : 'hidden',
+                    'width'        : self.getAttribute('tb-width', missing_ok=True),
+                },
+                'img': {
+                    'display': 'block',
+                    'width'  : '100%',
+                    'height' : 'auto',
+                },
+            },
+        }
+
+    def renderThumbnail(self):
+        carouselId = self.getAttribute('carouselId', missing_ok=True)
+        width, _ = widthParser(self.getAttribute('tb-width', missing_ok=True))
+        imgIndex = self.props['index'] + 1
+        cssClass = suffixCssClasses(
+            self.getAttribute('css-class', missing_ok=True),
+            'thumbnail',
+        )
+
+        a_attrs = self.html_attrs(
+            style='thumbnails.a',
+            href=f'#{imgIndex}',
+            target=self.getAttribute('target'),
+            class_=f'mj-carousel-thumbnail mj-carousel-{carouselId}-thumbnail mj-carousel-{carouselId}-thumbnail-{imgIndex} {cssClass}' # noqa: E501
+        )
+        img_attrs = self.html_attrs(
+            style='thumbnails.img',
+            src=self.getAttribute('thumbnails-src') or self.getAttribute('src'),
+            alt=self.getAttribute('alt'),
+            width=width,
+        )
+
+        return f'''
+            <a {a_attrs}>
+                <label {self.html_attrs(for_=f'mj-carousel-{carouselId}-radio-{imgIndex}')}>
+                    <img {img_attrs}/>
+                </label>
+            </a>
+        '''
+
+    def renderRadio(self):
+        index = self.props['index']
+        carouselId = self.getAttribute('carouselId', missing_ok=True)
+
+        return f'''
+            <input
+                {self.html_attrs(
+                    class_=f'mj-carousel-radio mj-carousel-{carouselId}-radio mj-carousel-{carouselId}-radio-{index + 1}',
+                    checked='checked' if index == 0 else None,
+                    type='radio',
+                    name=f'mj-carousel-radio-{carouselId}',
+                    id=f'mj-carousel-{carouselId}-radio-{index + 1}',
+                    style='radio.input',
+                )}
+            />
+        ''' # noqa: E501
+
+    def render(self):
+        href = self.getAttribute('href')
+        rel = self.getAttribute('rel')
+        width, _ = widthParser(self.context['containerWidth'])
+        index = self.props['index']
+
+        image = f'''
+            <img
+                {self.html_attrs(
+                    title=self.getAttribute('title'),
+                    src=self.getAttribute('src'),
+                    alt=self.getAttribute('alt'),
+                    style='images.img',
+                    width=width,
+                    border='0',
+                )}
+            />
+        '''
+
+        cssClass = self.getAttribute('css-class', missing_ok=True) or ''
+
+        return f'''
+            <div
+                {self.html_attrs(
+                    class_=f'mj-carousel-image mj-carousel-image-{index + 1} {cssClass}',
+                    style='images.firstImageDiv' if index == 0 else 'images.otherImageDiv',
+                )}
+            >
+                {
+                    f'<a {self.html_attrs(href=href, rel=rel, target="_blank")}>{image}</a>' if href else image
+                }
+            </div>
+        ''' # noqa: E501

commit 83451cbf9953142fe6a042df7de40bb364b4c9ff

fix `mj-carousel` so the rendering matches the generated HTML from upstream
Fixes: eec9ff4a ("add mjml-carousel")

mjml/elements/mj_carousel.py
@@ -1,7 +1,6 @@
 
 import random
 import string
-from itertools import repeat
 
 from ..helpers import msoConditionalTag, widthParser
 from ._base import BodyComponent
@@ -47,7 +46,7 @@ class MjCarousel(BodyComponent):
             'tb-border'               : '2px solid transparent',
             'tb-border-radius'        : '6px',
             'tb-hover-border-color'   : '#fead0d',
-            'tb-selected-border-color': '#ccc',
+            'tb-selected-border-color': '#cccccc',
         }
 
     carouselId = ''.join(random.choices(string.digits, k=16))
@@ -61,9 +60,10 @@ class MjCarousel(BodyComponent):
 
         def buildCssSelectors(parent, repeatCount, sibling):
             def _selector_str(i):
-                return f'{parent(i)} {repeat("+ * ", repeatCount(i))}+ {sibling(i)}'
+                repeated = '+*' * repeatCount(i)
+                return f'{parent(i)}{repeated}+{sibling(i)}'
             _selectors = [_selector_str(i) for i in range(length)]
-            return ','.join(_selectors)
+            return ', '.join(_selectors)
 
         carouselCss = f'''
             .mj-carousel {{
@@ -154,9 +154,10 @@ class MjCarousel(BodyComponent):
             }}
         ''' # noqa: E501
 
+        _sub_selector = '+*' * (length - 1)
         fallback = f'''
-            .mj-carousel noinput {{ display:block !important; }}
-            .mj-carousel noinput .mj-carousel-image-1 {{ display: block !important;  }}
+            .mj-carousel noinput {{ display: block !important; }}
+            .mj-carousel noinput .mj-carousel-image-1 {{ display: block !important; }}
             .mj-carousel noinput .mj-carousel-arrows,
             .mj-carousel noinput .mj-carousel-thumbnails {{ display: none !important; }}
 
@@ -169,15 +170,11 @@ class MjCarousel(BodyComponent):
                     display: none !important;
                 }}
 
-                {buildCssSelectors(
-                    lambda i: f'.mj-carousel-{carouselId}-radio-1:checked',
-                    lambda i: length - 1,
-                    lambda i: f'.mj-carousel-content .mj-carousel-{carouselId}-thumbnail-1'
-                )} {{
+                .mj-carousel-{carouselId}-radio-1:checked{_sub_selector}+.mj-carousel-content .mj-carousel-{carouselId}-thumbnail-1 {{
                     border-color: transparent;
                 }}
             }}
-        '''
+        ''' # noqa: E501
 
         return f'{carouselCss}\n{fallback}'
 