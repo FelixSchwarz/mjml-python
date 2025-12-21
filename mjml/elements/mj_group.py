FelixSchwarz/mjml-python

commit 9b1bc5943afe9743eb420c64ba018b9aef901ef4

implement <mj-group>

mjml/elements/mj_group.py
@@ -1,0 +1,165 @@
+
+from ._base import BodyComponent
+from ..helpers import strip_unit, widthParser
+from ..lib import merge_dicts
+
+
+__all__ = ['MjGroup']
+
+class MjGroup(BodyComponent):
+    @classmethod
+    def default_attrs(cls):
+        return {
+            'background-color' : '',
+            'direction'        : 'ltr',
+            'vertical-align'   : '',
+            'width'            : '',
+             # hidden / used by MjColumn
+            #'align'            : '',
+            'css-class'        : '',
+            # NOT declared but used by MjGroup in ".getChildContext()"
+            'padding'          : '',
+            'padding-left'     : '',
+            'padding-right'    : '',
+        }
+
+    # js: getStyles()
+    def get_styles(self):
+        return {
+            'div': {
+                'font-size'       : '0',
+                'line-height'     : '0',
+                'text-align'      : 'left',
+                'display'         : 'inline-block',
+                'width'           : '100%',
+                'direction'       : self.getAttribute('direction'),
+                'vertical-align'  : self.getAttribute('vertical-align'),
+                'background-color': self.getAttribute('background-color'),
+            },
+            'tdOutlook': {
+                'vertical-align'  : self.getAttribute('vertical-align'),
+                'width'           : self.getWidthAsPixel(),
+            }
+        }
+
+    def getChildContext(self):
+        parentWidth = float(strip_unit(self.context['containerWidth']))
+        nonRawSiblings = self.props['nonRawSiblings']
+        children = self.props['children']
+        paddingSize = self.getShorthandAttrValue('padding', 'left') + self.getShorthandAttrValue('padding', 'right')
+
+        containerWidth = self.getAttribute('width') or f'{(parentWidth / nonRawSiblings)}px'
+        parsedWidth, unit = widthParser(containerWidth, parseFloatToInt=False)
+
+        width_px = None
+        if unit == '%':
+            width_px = (parentWidth * parsedWidth) / 100 - paddingSize
+        else:
+            width_px = parsedWidth - paddingSize
+        containerWidth = f'{width_px}px'
+
+        return merge_dicts(self.context, {'containerWidth': containerWidth, 'nonRawSiblings': len(children)})
+
+    def getParsedWidth(self, toString=False):
+        nonRawSiblings = self.props['nonRawSiblings']
+        width = self.getAttribute('width') or f'{100 / nonRawSiblings}%'
+        width_unit = widthParser(width, parseFloatToInt=False)
+        if toString:
+            return str(width_unit)
+        return width_unit
+
+    def getWidthAsPixel(self):
+        containerWidth = self.context['containerWidth']
+        width_str = self.getParsedWidth(toString=True)
+        parsedWidth, unit = widthParser(width_str, parseFloatToInt=False)
+
+        if unit == '%':
+            width_px = (float(strip_unit(containerWidth)) * parsedWidth) / 100
+        else:
+            assert unit == 'px'
+            width_px = parsedWidth
+        return f'{width_px}px'
+
+    def getColumnClass(self):
+        parsedWidth, unit = self.getParsedWidth()
+        width_int = int(parsedWidth)
+        if unit == '%':
+            className = f'mj-column-per-{width_int}'
+        else:
+            # unit: 'px' or anything else (switch/default)
+            className = f'mj-column-px-{width_int}'
+
+        # Add className to media queries
+        addMediaQuery = self.context['addMediaQuery']
+        addMediaQuery(className, parsedWidth=parsedWidth, unit=unit)
+        return className
+
+    def render(self):
+        children = self.props['children']
+        nonRawSiblings = self.props['nonRawSiblings']
+        groupWidth = self.getChildContext()['containerWidth']
+        containerWidth = self.context['containerWidth']
+
+        def getElementWidth(width):
+            if not width:
+                containerWidth_int = int(containerWidth)
+                nr_non_raw_siblings = int(nonRawSiblings)
+                width_px = containerWidth_int / nr_non_raw_siblings
+                return f'{width_px}px'
+
+            width_unit = widthParser(width, parseFloatToInt=False)
+            if width_unit.unit == '%':
+                width_px = (100 * width_unit.parsedWidth) / groupWidth
+                return f'{width_px}px'
+            return str(width_unit)
+
+        classesName = f'{self.getColumnClass()} mj-outlook-group-fix'
+        if self.get_attr('css-class'):
+            classesName += f" {self.get_attr('css-class')}"
+
+        container_attrs = self.html_attrs(class_=classesName, style='div')
+        table_bgcolor = self.get_attr('background-color')
+        table_attrs = self.html_attrs(
+            bgcolor     = table_bgcolor if (table_bgcolor != 'none') else None,
+            border      = '0',
+            cellpadding = '0',
+            cellspacing = '0',
+            role = 'presentation',
+        )
+
+        def child_renderer(component):
+            if component.isRawElement():
+                return component.render()
+
+            if hasattr(component, 'getWidthAsPixel'):
+                width = component.getWidthAsPixel()
+            else:
+                component.getAttribute('width')
+            td_style = {
+                'align'         : component.get_attr('align'),
+                'vertical-align': component.get_attr('vertical-align'),
+                'width'         : getElementWidth(width),
+            }
+            td_attrs = component.html_attrs(style=td_style)
+            return f'''
+                <!--[if mso | IE]>
+                <td {td_attrs}>
+                  <![endif]-->
+                    {component.render()}
+                  <!--[if mso | IE]>
+                  </td>
+                  <![endif]-->
+            '''
+
+        return f'''<div {container_attrs}>
+            <!--[if mso | IE]>
+            <table {table_attrs}>
+              <tr>
+            <![endif]-->
+            {self.renderChildren(children, attributes={'mobileWidth': 'mobileWidth'}, renderer=child_renderer)}
+            <!--[if mso | IE]>
+              </tr>
+              </table>
+            <![endif]-->
+        </div>'''
+

mjml/core/registry.py
@@ -4,7 +4,7 @@ __all__ = []
 
 def _components():
     from ..elements import (MjButton, MjText, MjSection, MjColumn, MjBody,
-        MjImage, MjDivider)
+        MjGroup, MjImage, MjDivider)
     from ..elements.head import (MjAttributes, MjHead, MjStyle, MjTitle)
     components = {
         'mj-button': MjButton,
@@ -14,6 +14,7 @@ def _components():
         'mj-section': MjSection,
         'mj-column': MjColumn,
         'mj-body': MjBody,
+        'mj-group'  : MjGroup,
         # --- head components ---
         'mj-attributes': MjAttributes,
         'mj-head': MjHead,

mjml/elements/__init__.py
@@ -3,6 +3,7 @@ from .mj_body import *
 from .mj_button import *
 from .mj_column import *
 from .mj_divider import *
+from .mj_group import *
 from .mj_image import *
 from .mj_section import *
 from .mj_text import *

mjml/elements/_base.py
@@ -92,11 +92,28 @@ class BodyComponent(Component):
             # return childrens.map(child => jsonToXML(child)).join('\n')
             raise NotImplementedError
         sibling = len(childrens)
-        rawComponents = tuple(filter(lambda c: c.isRawElement(), _components().values()))
-        def has_non_raw_children(child):
-            has_same_tag = lambda c: (c.getTagName() == child.tagName)
-            return any(filter(has_same_tag, rawComponents))
-        _nonRawSiblings = tuple(filter(has_non_raw_children, childrens))
+
+        # code to calculate "nonRawSiblings" seemed too compressed/complicated
+        # to me (traded readability for fewer lines of code) so the Python code
+        # looks quite a bit different. However the final HTML looks the same
+        # so I guess I got it right somehow :-)
+        #
+        # upstream:
+        # const rawComponents = filter(components, c => c.isRawElement())
+        # const nonRawSiblings = childrens.filter(
+        #  child => !find(rawComponents, c => c.getTagName() === child.tagName),
+        #).length
+        raw_tag_names = set()
+        for tag_name, component in _components().items():
+            if component.isRawElement():
+                raw_tag_names.add(tag_name)
+        is_raw_element = lambda c: (c['tagName'] in raw_tag_names)
+
+        _nonRawSiblings = []
+        for child in childrens:
+            if (child is None) or is_raw_element(child):
+                continue
+            _nonRawSiblings.append(child)
         nonRawSiblings = len(_nonRawSiblings)
 
         output = ''

mjml/elements/mj_column.py
@@ -38,6 +38,8 @@ class MjColumn(BodyComponent):
             'css-class'        : '',
             # not defined upstream but used?
             'mobileWidth'      : '',
+            # not declared but used by MjGroup
+            'align'            : '',
         }
 
     def get_styles(self):
@@ -91,8 +93,9 @@ class MjColumn(BodyComponent):
         mobileWidth = self.getAttribute('mobileWidth')
         if mobileWidth != 'mobileWidth':
             return '100%'
-        elif width is None:
-            return f'{parse_int(100 / nonRawSiblings)}%'
+        # upstream uses "width === undefined" but we also need to handle width=''
+        elif not width:
+            return f'{int(100 / nonRawSiblings)}%'
 
         parsedWidth, unit = widthParser(width, parseFloatToInt=False)
         if unit == '%':
@@ -137,8 +140,7 @@ class MjColumn(BodyComponent):
 
     def getParsedWidth(self, toString=False):
         this = self
-        # FIXME: "or 1" not upstream
-        nonRawSiblings = this.props['nonRawSiblings'] or 1
+        nonRawSiblings = this.props['nonRawSiblings']
         width = this.getAttribute('width') or f'{100 / nonRawSiblings}%'
 
         width_unit = widthParser(width, parseFloatToInt=False)
@@ -148,8 +150,7 @@ class MjColumn(BodyComponent):
 
     def getChildContext(self):
         parentWidth = float(strip_unit(self.context['containerWidth']))
-        # FIXME: "or 1" not upstream
-        nonRawSiblings = self.props['nonRawSiblings'] or 1
+        nonRawSiblings = self.props['nonRawSiblings']
         box_widths = self.getBoxWidths()
         borders = box_widths['borders']
         paddings = box_widths['paddings']

tests/testdata/mj-group-expected.html
@@ -1,0 +1,209 @@
+<!-- FILE: ../mjml-python/tests/testdata/mj-group.mjml -->
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+  <title>
+  </title>
+  <!--[if !mso]><!-- -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--<![endif]-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style type="text/css">
+    #outlook a {
+      padding: 0;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      -webkit-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+
+    table,
+    td {
+      border-collapse: collapse;
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+    }
+
+    img {
+      border: 0;
+      height: auto;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+      -ms-interpolation-mode: bicubic;
+    }
+
+    p {
+      display: block;
+      margin: 13px 0;
+    }
+  </style>
+  <!--[if mso]>
+        <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+        </xml>
+        <![endif]-->
+  <!--[if lte mso 11]>
+        <style type="text/css">
+          .mj-outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+  <!--[if !mso]><!-->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+  </style>
+  <!--<![endif]-->
+  <style type="text/css">
+    @media only screen and (min-width:480px) {
+      .mj-column-per-100 {
+        width: 100% !important;
+        max-width: 100%;
+      }
+
+      .mj-column-per-50 {
+        width: 50% !important;
+        max-width: 50%;
+      }
+    }
+  </style>
+  <style type="text/css">
+    @media only screen and (max-width:480px) {
+      table.mj-full-width-mobile {
+        width: 100% !important;
+      }
+
+      td.mj-full-width-mobile {
+        width: auto !important;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <div style="">
+    <!--[if mso | IE]>
+      <table
+         align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600"
+      >
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+    <div style="margin:0px auto;max-width:600px;">
+      <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+        <tbody>
+          <tr>
+            <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+              <!--[if mso | IE]>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                
+        <tr>
+      
+            <td
+               class="" style="width:600px;"
+            >
+          <![endif]-->
+              <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0;line-height:0;text-align:left;display:inline-block;width:100%;direction:ltr;">
+                <!--[if mso | IE]>
+        <table
+           border="0" cellpadding="0" cellspacing="0" role="presentation"
+        >
+          <tr>
+        
+              <td
+                 style="vertical-align:top;width:300px;"
+              >
+              <![endif]-->
+                <div class="mj-column-per-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:50%;">
+                  <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                    <tr>
+                      <td align="center" style="font-size:0px;padding:0;word-break:break-word;">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                          <tbody>
+                            <tr>
+                              <td style="width:137px;">
+                                <img height="185" src="https://mjml.io/assets/img/easy-and-quick.png" style="border:0;display:block;outline:none;text-decoration:none;height:185px;width:100%;font-size:13px;" width="137" />
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                        <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:center;color:#000000;">
+                          <h2>Easy and quick</h2>
+                          <p>Write less code, save time and code more efficiently with MJML’s semantic syntax.</p>
+                        </div>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <!--[if mso | IE]>
+              </td>
+              
+              <td
+                 style="vertical-align:top;width:300px;"
+              >
+              <![endif]-->
+                <div class="mj-column-per-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:50%;">
+                  <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                    <tr>
+                      <td align="center" style="font-size:0px;padding:0;word-break:break-word;">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                          <tbody>
+                            <tr>
+                              <td style="width:166px;">
+                                <img height="185" src="https://mjml.io/assets/img/responsive.png" style="border:0;display:block;outline:none;text-decoration:none;height:185px;width:100%;font-size:13px;" width="166" />
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                        <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:center;color:#000000;">
+                          <h2>Responsive</h2>
+                          <p>MJML is responsive by design on most-popular email clients, even Outlook.</p>
+                        </div>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <!--[if mso | IE]>
+              </td>
+              
+          </tr>
+          </table>
+        <![endif]-->
+              </div>
+              <!--[if mso | IE]>
+            </td>
+          
+        </tr>
+      
+                  </table>
+                <![endif]-->
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!--[if mso | IE]>
+          </td>
+        </tr>
+      </table>
+      <![endif]-->
+  </div>
+</body>
+
+</html>

tests/testdata/mj-group.mjml
@@ -1,0 +1,22 @@
+<mjml>
+  <mj-body>
+    <mj-section>
+      <mj-group>
+        <mj-column>
+          <mj-image width="137px" height="185px" padding="0" src="https://mjml.io/assets/img/easy-and-quick.png" />
+          <mj-text align="center">
+            <h2>Easy and quick</h2>
+            <p>Write less code, save time and code more efficiently with MJML’s semantic syntax.</p>
+          </mj-text>
+        </mj-column>
+        <mj-column>
+          <mj-image width="166px" height="185px" padding="0" src="https://mjml.io/assets/img/responsive.png" />
+          <mj-text align="center">
+            <h2>Responsive</h2>
+            <p>MJML is responsive by design on most-popular email clients, even Outlook.</p>
+          </mj-text>
+        </mj-column>
+      </mj-group>
+    </mj-section>
+  </mj-body>
+</mjml>

tests/upstream_alignment_test.py
@@ -21,6 +21,7 @@ class UpstreamAlignmentTest(TestCase):
         'mj-title',
         'mj-style',
         'mj-attributes',
+        'mj-group',
     )
     def test_ensure_same_html(self, test_id):
         mjml_filename = f'{test_id}.mjml'

commit 92fdd2a347a8fb0467a84dba6c911983c2801466

fix rendering for `mj-group`
Fixes: 9b1bc594 ("implement <mj-group>")

mjml/elements/mj_group.py
@@ -129,7 +129,7 @@ class MjGroup(BodyComponent):
         container_attrs = self.html_attrs(class_=classesName, style='div')
         table_bgcolor = self.get_attr('background-color')
         table_attrs = self.html_attrs(
-            bgcolor     = table_bgcolor if (table_bgcolor != 'none') else None,
+            bgcolor     = table_bgcolor if (table_bgcolor and table_bgcolor != 'none') else None,
             border      = '0',
             cellpadding = '0',
             cellspacing = '0',