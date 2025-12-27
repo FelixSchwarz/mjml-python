92148bb3bd9c7bb2b9edb3e376e8601776517d76
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2025-12-27 11:56:14 +0100

    add CLI parameter `--config.keepComments=true/false` to control whether comments should be kept

--- mjml/scripts/mjml.py
+++ mjml/scripts/mjml.py
@@ -8,12 +8,14 @@ Usage:
 
 Options:
   --template-dir=<path>    base dir for mj-include (default: path of mjml file)
+  --config.keepComments=False  whether comments in mjml should be present in the generated html (default: true)
 """
 # ruff: noqa: E501
 
 import sys
 from io import BytesIO
 from pathlib import Path
+from typing import Union
 
 from docopt import docopt
 
@@ -25,14 +27,19 @@ def main():
     mjml_filename = arguments['<MJML-FILE>']
     output_filename = arguments['<OUTPUT-FILE>']
     template_dir = arguments['--template-dir']
+    keep_comments_str = arguments['--config.keepComments']
+    keep_comments = _parse_bool(keep_comments_str, default=True)
+    if keep_comments is None:
+        sys.stderr.write("value for --config.keepComments should be either true or false\n")
+        sys.exit(1)
 
     if mjml_filename == '-':
         stdin = sys.stdin.buffer
         mjml_fp = BytesIO(stdin.read())
-        result = mjml_to_html(mjml_fp, template_dir=template_dir)
+        result = mjml_to_html(mjml_fp, template_dir=template_dir, keep_comments=keep_comments)
     else:
         with Path(mjml_filename).open('rb') as mjml_fp:
-            result = mjml_to_html(mjml_fp, template_dir=template_dir)
+            result = mjml_to_html(mjml_fp, template_dir=template_dir, keep_comments=keep_comments)
     assert not result.errors, result.errors
 
     html_str = result.html
@@ -45,6 +52,21 @@ def main():
         #   UnicodeEncodeError: 'charmap' codec can't encode character '\ufb02' in position …: character maps to <undefined>
         html_bytes = html_str.encode('utf8')
         sys.stdout.buffer.write(html_bytes)
+
+
+def _parse_bool(value: Union[str, None], *, default: bool) -> Union[bool, None]:
+    if value is None:
+        return default
+    truthy = {'true', '1', 'yes', 'y'}
+    falsey = {'false', '0', 'no', 'n'}
+
+    value_lower = value.strip().lower()
+    if value_lower in truthy:
+        return True
+    elif value_lower in falsey:
+        return False
+    else:
+        return None
 
 
 if __name__ == '__main__':
