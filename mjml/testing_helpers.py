8ec33e237f764cad9717058985685d3db2e1db02
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2026-08-26 18:19:47 +0200

    tests: fix typing issues

--- mjml/testing_helpers.py
+++ mjml/testing_helpers.py
@@ -8,11 +8,10 @@ __all__ = ['get_mjml_fp', 'load_expected_html']
 
 TESTDATA_DIR = Path(__file__).parent / '..' / 'tests' / 'testdata'
 
-def load_expected_html(test_id, suffix: Union[str, None] = None) -> bytes:
+def load_expected_html(test_id, suffix: Union[str, None] = None) -> str:
     html_filename = f'{test_id}-expected{suffix or ""}.html'
-    with (TESTDATA_DIR / html_filename).open('rb') as html_fp:
-        expected_html = html_fp.read()
-    return expected_html
+    html_path = TESTDATA_DIR / html_filename
+    return html_path.read_text()
 
 @contextmanager
 def get_mjml_fp(test_id, json=False):
